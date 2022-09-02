71f51733c0cfd39a5662959eaf4cca9c244f0491
Shubham-Singh1810 <67862668+Shubham-Singh1810@users.noreply.github.com>
2022-09-02 12:14:57 +0530

    Update App.js
    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
     <h1 data-ns-test="project-name">Instagreen</h1>
-    <p data-ns-test="project-description">Instagreen Would be a instagreen clone app which have all the functinality of instagram </p>
+    <p data-ns-test="project-description">Instagreen Would be a instagreen clone app which have all the functinality of instagram using firebase as a database</p>
     </div>
   )
 }
